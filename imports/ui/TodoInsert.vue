6e2cd4b2302585bdbf20fa4a2e6d8b9c5dfebc5b
Steven Dao <iamstevendao@users.noreply.github.com>
2017-11-27 20:48:09 +1100

    fix header and add check

--- imports/ui/TodoInsert.vue
+++ imports/ui/TodoInsert.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .row
   .input-group
-    input.form-control(@keyup.enter="submit", v-model="text", placeholder="add new task...")
+    input.form-control(@keyup.enter="submit", v-model="text", placeholder="add a new task...")
     span.input-group-btn
       button.btn.btn-primary(@click="submit") add
 </template>
